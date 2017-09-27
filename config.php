4e4c21a2cb53d19a1acc70fa4d9b1f7679ef9e49
Robbie <txt3rob@gmail.com>
2017-09-27 10:32:33 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Some general settings
+ */
+date_default_timezone_set('UTC');
+set_time_limit(0);
+ini_set("memory_limit","-1");
+/**
+ * Error reporting settings
+ */
+ini_set('display_errors', 'On');
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING);
+/**
+ * Database related configuration
+ * For DB_DRIVER use mysql for MySQL or pgsql for PostgreSQL
+ */
+define('DB_DRIVER',	    'mysql');
+define('DB_HOST',	    '192.168.1.71');
+define('DB_USERNAME',	'docker_massscan');
+define('DB_PASSWORD', 	'docker_massscan');
+define('DB_DATABASE', 	'docker_massscan');
